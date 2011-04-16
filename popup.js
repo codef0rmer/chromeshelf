fb3dd1a43f9d7c3960acd864d89a1cb1de06cbac
Amit Gharat <amit.2006.it@gmail.com>
2011-04-16 20:25:57 +0530

    Chromeshelf first commit

---
+++ popup.js
@@ -0,0 +1,90 @@
+DB = function() {
+	this.init();
+	return this;
+};
+
+DB.prototype = {
+    ebooks: [],
+    index: 0,
+
+    init: function() {
+        this.load();
+    },
+
+    create: function(ebook) {
+        if (ebook == null) throw new TypeError('Missing ebook parameter');
+
+        ebook.id = this.index++;
+
+        this.ebooks.push(ebook);
+        this.save();
+    },
+
+    remove: function(ebook) {
+        if (ebook == null) throw new TypeError('Missing ebook parameter');
+        if (ebook.id == null) throw new TypeError('Missing ebook.id property');
+        var self = this;
+
+        this.ebooks.forEach(function(t, i) {
+            if (t.id == ebook.id) self.ebooks.splice(i, 1);
+        });
+
+        this.save();
+    },
+
+    load: function() {
+        this.ebooks = (localStorage.ebooks ? JSON.parse(localStorage.ebooks) : []);
+        this.index = localStorage.index ? localStorage.index : 0;
+    },
+
+    save: function() {
+        localStorage.ebooks = this.ebooks ? JSON.stringify(this.ebooks) : null;
+        localStorage.index = this.index;
+    }
+};
+
+Chromeshelf = function() { return this; }
+Chromeshelf.prototype = {
+    textbox: null,
+
+    init: function() {
+        this.textbox = document.getElementById('add');
+        this.renderView();
+    },
+
+    newEbook: function(e) {
+        if (e != null && e.which == 13 && this.textbox.value != "") { // enter key
+            var url = this.textbox.value;
+            var filename = url.substring(url.lastIndexOf('/')+1);
+            db.create({
+                url: url,
+                filename: filename,
+                created: new Date()
+            });
+            this.textbox.value = '';
+            chromeshelf.renderView();
+        }
+    },
+
+    removeEbook: function(id) {
+        db.remove({ id: id });
+        chromeshelf.renderView();
+        return;
+    },
+
+    renderView: function() {
+        $('div#results').html('');
+        $.each(JSON.parse(localStorage.ebooks), function(i){
+            var filename = $.URLDecode(this.filename);
+            var url = this.url;
+            url = url.indexOf('file://') ==  0 ? url : 'file://'+url;
+            $('div#results').append(
+                "<div class='item' id="+this.id+"><a style='cursor:pointer;' onclick=\"javascript:chrome.tabs.create({url: '"+url+"'});\" target='_blank' class='title' title='"+filename+"'>"+filename.substr(0, 50)+"</a><button class='remove' onclick='javascript:chromeshelf.removeEbook(this.parentNode.id)' title='Remove ebook'>Remove</button></div>"
+            );
+        });
+    },
+};
+var db = new DB();
+var chromeshelf = new Chromeshelf();
+
+window.onload = function() { chromeshelf.init(); };
